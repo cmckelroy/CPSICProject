5a67d22520bf4e90276fef71aa2b1c01dfe78e4e
Matt S. Carson <carsonm940120@gmail.com>
2020-04-27 13:19:18 -0400

    Absolute time range

--- src/AbsoluteTimeRange.cpp
+++ src/AbsoluteTimeRange.cpp
@@ -2,6 +2,8 @@
 
 void AbsoluteTimeRange::print()
 {
+	std::cout << "Start time: " << startTime << std::endl;
+	std::cout << "End time:   " << endTime << std::endl;
 }
 
 bool AbsoluteTimeRange::operator==(AbsoluteTimeRange& rhs)
